9187539f05453bb5789d62d9e5f8a1de8c90bc7e
LionStar303 <152593425+LionStar303@users.noreply.github.com>
2024-12-02 13:24:01 +0100

    day 02 working

--- 2024/day_02/src/Day02.java
+++ 2024/day_02/src/Day02.java
@@ -13,7 +13,7 @@ public class Day02 {
         System.out.println("...starte...");
 
         String demoPath = "2024\\day_02\\" + "demo-input.txt";
-        String path = "2024\\day_02\\" + "testinput.txt";
+        String path = "2024\\day_02\\" + "input.txt";
         readInput(new File(path));
 
         // Part 1
@@ -37,7 +37,8 @@ public class Day02 {
             for(String s : numArray) {
                 nums.add(Integer.parseInt(s));
             }
-            if(isSafe2(nums)) result2++;
+            if(isSafe3(nums)) result2++;
+            if(isSafe3(nums) != isSafe2(nums)) System.out.println(nums);
         }
         
         System.out.println("result of part 2: " + result2);
@@ -57,24 +58,35 @@ public class Day02 {
         return true;
     }
 
+    private static boolean isSafe3(ArrayList<Integer> nums) {
+        for(int i = 0; i < nums.size(); i++) {
+            ArrayList<Integer> nums2 = new ArrayList<>(nums);
+            nums2.remove(i);
+            if(isSafe(nums2)) return true;
+        }
+        return false;
+    }
+
     private static boolean isSafe2(ArrayList<Integer> nums) {
         boolean inc = (nums.get(1) - nums.get(0)) > 0;
         for(int i = 1; i < nums.size(); i++) {
             if(((nums.get(i) - nums.get(i-1)) > 0) != inc) {
                 ArrayList<Integer> nums2 = new ArrayList<Integer>(nums);
+                ArrayList<Integer> nums3 = new ArrayList<Integer>(nums);
                 nums2.remove(0);
-                if(isSafe(nums2)) {
-                    return true;
-                }
+                nums3.remove(1);
+                if(isSafe(nums2)) return true;
+                if(isSafe(nums3)) return true;
                 nums.remove(i);
                 return isSafe(nums);
             }
             if((Math.abs(nums.get(i) - nums.get(i-1)) < 1) || (Math.abs(nums.get(i) - nums.get(i-1)) > 3)) {
                 ArrayList<Integer> nums2 = new ArrayList<Integer>(nums);
+                ArrayList<Integer> nums3 = new ArrayList<Integer>(nums);
                 nums2.remove(0);
-                if(isSafe(nums2)) {
-                    return true;
-                }
+                nums3.remove(1);
+                if(isSafe(nums2)) return true;
+                if(isSafe(nums3)) return true;
                 nums.remove(i);
                 return isSafe(nums);
             }
