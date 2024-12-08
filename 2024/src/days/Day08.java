4d832dd5577d1b4e02bcb03aff136c8890ea7573
LionStar303 <p.siegel.15@gmail.com>
2024-12-08 16:22:48 +0100

    day08 teil 2

--- 2024/src/days/Day08.java
+++ 2024/src/days/Day08.java
@@ -17,12 +17,12 @@ public class Day08 {
         String path = folderPath + "input08.txt";
         readInput(new File(path));
 
-        // Part 1
         ArrayList<char[]> antinodes = new ArrayList<char[]>();
         for(int line = 0; line < input.size(); line ++) {
             antinodes.add(input.get(line).replaceAll("\\w", ".").toCharArray());
         }
 
+        // Part 1
         // x1,y1 Koordinaten der ersten Antenne
         for(int y1 = 0; y1 < input.size(); y1++) {
             for(int x1 = 0; x1 < input.get(y1).length(); x1++) {
@@ -35,28 +35,52 @@ public class Day08 {
                         if((x1 == x2 && y1 == y2) || input.get(y2).charAt(x2) != antenna1) continue; // continue wenn keine, falsche oder selbe Antenne
                         int dx = x1 - x2;
                         int dy = y1 - y2;
-                        // aeussere Antinodes markieren wenn im grid
+                        // Antinodes markieren wenn im grid
                         try { antinodes.get(y1+dy)[x1+dx] = '#'; } catch (IndexOutOfBoundsException e) { }
                         try { antinodes.get(y2-dy)[x2-dx] = '#'; } catch (IndexOutOfBoundsException e) { }
-                        // innere Antinodes markieren
-                        /*if(dx % 3 == 0 && dy % 3 == 0) {
-                            antinodes.get(y1-dy/3)[x1-dx/3] = '#';
-                            antinodes.get(y2+dy/3)[x2+dx/3] = '#';
-                        }*/
                     }
                 }
             }
         }
-        for(char[] carr : antinodes) {
-            System.out.println(carr);
-            for(char c : carr) if(c == '#') result1++;
-        }
+        for(char[] carr : antinodes) for(char c : carr) if(c == '#') result1++;
         System.out.println("result of part 1: " + result1);
 
         // Part 2
-        
+        // x1,y1 Koordinaten der ersten Antenne
+        for(int y1 = 0; y1 < input.size(); y1++) {
+            for(int x1 = 0; x1 < input.get(y1).length(); x1++) {
+                char antenna1 = input.get(y1).charAt(x1);
+                if(antenna1 == '.') continue; // continue wenn keine Antenne
+
+                // x2,y2 Koordinaten der zweiten Antenne
+                for(int y2 = y1; y2 < input.size(); y2++) {
+                    for(int x2 = 0; x2 < input.get(y2).length(); x2++) {
+                        if((x1 == x2 && y1 == y2) || input.get(y2).charAt(x2) != antenna1) continue; // continue wenn keine, falsche oder selbe Antenne
+                        int dx = x1 - x2, dy = y1 - y2;
+                        dx /= gcd(dx, dy);
+                        dy /= gcd(dx, dy);
+                        int ndx = dx, ndy = dy;
+                        // Antinodes markieren wenn im grid
+                        while((y1-ndy < antinodes.size() && y1-ndy >= 0 && x1-ndx < antinodes.get(y1-ndy).length && x1-ndx >= 0) ||
+                              (y2+ndy < antinodes.size() && y2+ndy >= 0 && x2+ndx < antinodes.get(y2+ndy).length && x2+ndx >= 0)) {
+                            try { antinodes.get(y1-ndy)[x1-ndx] = '#'; } catch (IndexOutOfBoundsException e) { }
+                            try { antinodes.get(y2+ndy)[x2+ndx] = '#'; } catch (IndexOutOfBoundsException e) { }
+                            ndx += dx;
+                            ndy += dy;
+                        }
+                    }
+                }
+            }
+        }
+        for(char[] carr : antinodes) for(char c : carr) if(c == '#') result2++;
+        for(char[] carr : antinodes) System.out.println(carr);
         System.out.println("result of part 2: " + result2);
         System.out.println("...success...");
+    }
+
+    private static int gcd(int a, int b) {
+        if(b == 0) return Math.abs(a);
+        else return gcd(b, a % b);
     }
     
     private static void readInput(File file) {
