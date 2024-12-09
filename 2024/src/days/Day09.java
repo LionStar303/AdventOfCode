14f1a525fdffd60ff73ee72639c243f2fc3d518f
LionStar303 <152593425+LionStar303@users.noreply.github.com>
2024-12-09 12:30:20 +0100

    day09 part 2

--- 2024/src/days/Day09.java
+++ 2024/src/days/Day09.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Day09 {
 
     private static long result1 = 0;
-    private static int result2 = 0;
+    private static long result2 = 0;
     private static char[] input;
 
     public static void main(String[] args) {
@@ -16,15 +16,25 @@ public class Day09 {
         String folderPath = "2024\\input\\";
         String path = folderPath + "input09.txt";
         readInput(new File(path));
-        ArrayList<String> disk = new ArrayList<>();
 
-        // Part 1
         // einlesen
+        ArrayList<String> disk = new ArrayList<>();
         for(int i = 0; i < input.length; i++) {
             if(i % 2 == 0) for(int j = 0; j < input[i]-'0'; j++) disk.add(String.valueOf(i / 2));
             else for(int j = 0; j < input[i]-'0'; j++) disk.add(".");
         }
 
+        // Part 1
+        result1 = part1(new ArrayList<>(disk));
+        System.out.println("result of part 1: " + result1);
+
+        // Part 2
+        result2 = part2(new ArrayList<>(disk));
+        System.out.println("result of part 2: " + result2);
+        System.out.println("...success...");
+    }
+
+    private static long part1(ArrayList<String> disk) {
         // komprimieren
         int writePointer = 0;
         int readPointer = disk.size()-1;
@@ -38,14 +48,37 @@ public class Day09 {
             disk.removeLast();
             writePointer++; readPointer--;
         }
+        long res = 0;
+        for(int i = 0; i < disk.size(); i++) res += i * Integer.parseInt(disk.get(i));
+        return res;
+    }
 
-        for(int i = 0; i < disk.size(); i++) result1 += i * Integer.parseInt(disk.get(i));
-        System.out.println("result of part 1: " + result1);
-
-        // Part 2
-        
-        System.out.println("result of part 2: " + result2);
-        System.out.println("...success...");
+    private static long part2(ArrayList<String> disk) {
+        // komprimieren
+        int readPointer = disk.size()-1;
+        while(readPointer > 0) {
+            // nächste Datei finden
+            while(disk.get(readPointer) == ".")  readPointer--;
+            // Größe der Datei ermitteln
+            int len = 0, id = Integer.parseInt(disk.get(readPointer));
+            while(readPointer >= 0 && disk.get(readPointer) != "." && Integer.parseInt(disk.get(readPointer)) == id)  { readPointer--; len++; }
+            // ausreichen großen Block suchen
+            int free = 0, writePointer = 0;
+            while(free < len && writePointer < readPointer+1) {
+                if(disk.get(writePointer) == ".") free++;
+                else free = 0;
+                writePointer++;
+            }
+            if(free < len) continue; // weiter wenn kein ausreichend großer Block
+            writePointer -= len;
+            for(int i = 0; i < len; i++) {
+                disk.set(writePointer+i, disk.get(readPointer+i+1));
+                disk.set(readPointer+i+1, ".");
+            }
+        }
+        long res = 0;
+        for(int i = 0; i < disk.size(); i++) if(disk.get(i) != ".") res += i * Integer.parseInt(disk.get(i));
+        return res;
     }
     
     private static void readInput(File file) {
